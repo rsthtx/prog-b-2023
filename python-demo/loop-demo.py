3707f8cecf70f3f0ee7b687889d0a4c70ac29e1f
Rasmus Stougaard <rasmus.stougaard@gmail.com>
2023-10-09 12:45:58 +0200

    Add examples

---
+++ python-demo/loop-demo.py
@@ -0,0 +1,4 @@
+spam = 0
+while spam < 10:
+    print('Hello, world. ' + str(spam))
+    spam = spam + 1
---
+++ python-demo/vampire-nested-if.py
@@ -0,0 +1,14 @@
+name = 'Carol'
+age = 101
+
+if name == 'Alice':
+    print('Hi, Alice.')
+else:
+    if age < 12:
+        print('You are not Alice, kiddo.')
+    else:
+        if age > 2000:
+            print('Unlike you, Alice is not an undead, immortal vampire.')
+        else:
+            if age > 100:
+                print('You are not Alice, grannie.')
